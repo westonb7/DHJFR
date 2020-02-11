4e891ee66eee6e75a02e156c0252632970137bed
Weston Bodily <westonbodily7z@gmail.com>
2020-02-11 12:54:50 -0700

    Scrapped non-working code. Started anew with a default react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the test website.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/westonb7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
